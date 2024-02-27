481a4267d9d2f302f6193fbdd70f5fb434b728fc
ChristofferS22R <95274166+DamrongS@users.noreply.github.com>
2024-02-27 19:30:47 +0100

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -1,30 +1,60 @@
+let Gravity = 0.98;
+
 let plr;
-let mapp;
+
+let GroundLevel;
+
+let playerSprites = [];
+
+function preload()
+{
+  playerSprites.push(loadImage("assets/marshmallow.png"));
+  playerSprites.push(loadImage("assets/marshmallowWalk.gif"));
+  playerSprites.push(loadImage("assets/marshmallowJump.png"));
+  playerSprites.push(loadImage("assets/marshmallowFall.png"));
+}
 
 function setup() 
 {
-  createCanvas(800, 600);
-  plr = new Player(width/2, height/1.5, "Normal")
-  mapp = new Map(0, 0, "Normal")
+  createCanvas(1200, 800);
+  noSmooth();
+  GroundLevel = height/1.5;
+  plr = new Player(width/2, height/1.5, "White")
 }
 
 function draw() 
 {
-  mapp.show();
+  background(220);
 
   plr.show();
   plr.update();
+
 }
 
 function keyPressed()
 {
   if(keyCode == LEFT_ARROW)
   {
-    plr.Left(0.1)
+    plr.Left(1);
+    if(!plr.isJumping)
+      {
+        plr.setState(1);
+      }
+    plr.holdingLeft = true;
   }
   if(keyCode == RIGHT_ARROW)
   {
-    plr.Right(0.1)
+    plr.Right(1);
+    if(!plr.isJumping)
+      {
+        plr.setState(1);
+      }
+    plr.holdingRight = true;
+  }
+  else if (keyCode === 32 || keyCode == UP_ARROW) 
+  {
+    plr.setState(2);
+    plr.jump();
   }
 }
 
@@ -32,10 +62,40 @@ function keyReleased()
 {
   if(keyCode == LEFT_ARROW)
   {
-    plr.Left(0)
+    plr.Left(0, false);
+    plr.setState(0);
+    plr.holdingLeft = false;
+    if(plr.getBool()[1] == true)
+    {
+      this.changedSprite = false
+      plr.Right(1);
+      if(!plr.isJumping)
+      {
+        plr.setState(1);
+      }
+      plr.holdingRight = true;
+    }
   }
   if(keyCode == RIGHT_ARROW)
   {
-    plr.Right(0)
+    this.changedSprite = false
+    plr.Right(0, false);
+    plr.holdingRight = false;
+    plr.setState(0);
+    if(plr.getBool()[0] == true)
+    {
+      this.changedSprite = true
+      plr.Left(1, true);
+      if(!plr.isJumping)
+      {
+        plr.setState(1);
+      }
+      plr.holdingLeft = true;
+    }
+  }
+  else if (keyCode === 32 || keyCode == UP_ARROW) 
+  {
+    plr.setState(3);
+    this.changedSprite = false
   }
 }
