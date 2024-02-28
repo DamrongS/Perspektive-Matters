150f08a148a40903340773cb70d0eeae33ea3ff4
ChristofferS22R <95274166+DamrongS@users.noreply.github.com>
2024-02-28 19:16:09 +0100

    Paralax

--- Paralax.js
+++ Paralax.js
@@ -8,16 +8,35 @@ class Paralax
 
     show(offset)
     {
-        push();
-        translate(0, 0)
-        imageMode(CENTER)
+        if(this.backdrop == "Forest")
+        {
+            push();
+            translate(0, 0)
+            imageMode(CENTER)
+    
+            image(layer3Img, (this.speed * offset) / 78, height/10, width*1, height*1)
+    
+            image(layer2Img, (this.speed * offset) / 64, height/3, width*1, height*1)
+    
+            image(layer1Img, (this.speed * offset) / 256, 0, width*2, height*2)
+            pop();
+        }
+        else if(this.backdrop == "SomethingElse")
+        {
+            push();
+            translate(0, 0)
 
-        image(layer3Img, (this.speed * offset) / 78, height/10, width*1, height*1)
+            //change the background backdrop
 
-        image(layer2Img, (this.speed * offset) / 64, height/3, width*1, height*1)
-
-        image(layer1Img, (this.speed * offset) / 256, 0, width*2, height*2)
-        pop();
+            // imageMode(CENTER)
+    
+            // image(layer3Img, (this.speed * offset) / 78, height/10, width*1, height*1)
+    
+            // image(layer2Img, (this.speed * offset) / 64, height/3, width*1, height*1)
+    
+            // image(layer1Img, (this.speed * offset) / 256, 0, width*2, height*2)
+            pop();
+        }
     }
 
 }
